b8853767f482fcc6d4e39af3ae5ceb2549f3cb34
Chantreck <minetvtc@yandex.ru>
2021-06-10 22:34:26 +0700

    Добавил мини-игру на поиск предметов

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Interactable : MonoBehaviour
+{
+    public bool isInRange;
+    public KeyCode interactKey;
+    public UnityEvent interactAction;
+    public GameObject message;
+    public GameObject curObject;
+
+    void Update()
+    {
+        if (isInRange)
+        {
+            if (Input.GetKeyDown(interactKey))
+            {
+                interactAction.Invoke();
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            message.SetActive(true);
+            isInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            message.SetActive(false);
+            isInRange = false;
+        }
+    }
+}
---
+++ Assets/Scripts/WardrobeController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WardrobeController : MonoBehaviour
+{
+    public static bool isSolved = false;
+    public GameObject message;
+
+    public void OpenWardrobe()
+    {
+        if (!isSolved)
+        {
+            SceneManager.LoadSceneAsync("HiddenObjectsGame", LoadSceneMode.Additive);
+        }
+        else
+        {
+
+        }
+    }
+}
